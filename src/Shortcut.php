b0eba676c7d950be490caf1ce5c5abcf51059ac9
Wolfy-J <wolfy.jd@gmail.com>
2018-08-26 23:29:29 +0300

    bootloader

--- src/Shortcut.php
+++ src/Shortcut.php
@@ -11,7 +11,33 @@ namespace Spiral\Jobs;
 /**
  * Runs all the jobs in the same process.
  */
-class Shortcut
+class Shortcut implements JobsInterface
 {
+    private $handler;
 
+    public function __construct(Handler $handler)
+    {
+        $this->handler = $handler;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function push(JobInterface $job, Options $options = null): string
+    {
+//        try {
+//            if (empty($options)) {
+//                $options = new Options();
+//            }
+//
+//            return $this->rpc->call(self::RR_SERVICE . '.Push', [
+//                'job'      => get_class($job),
+//                'pipeline' => $this->config->jobPipeline(get_class($job)),
+//                'payload'  => $job,
+//                'options'  => $options
+//            ]);
+//        } catch (RoadRunnerException|\Throwable $e) {
+//            throw new JobException($e->getMessage(), $e->getCode(), $e);
+//        }
+    }
 }
