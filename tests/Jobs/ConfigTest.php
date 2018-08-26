3f87d805024df7f49c6f9c99226f338422e2926e
Wolfy-J <wolfy.jd@gmail.com>
2018-08-26 23:36:18 +0300

    shortcut

---
+++ tests/Jobs/ConfigTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+
+namespace Spiral\Jobs\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Spiral\Jobs\Configs\JobsConfig;
+use Spiral\Jobs\Tests\Fixtures\LocalJob;
+
+class ConfigTest extends TestCase
+{
+    public function testConfig()
+    {
+        $config = new JobsConfig([
+            'pipelines'       => [
+                LocalJob::class => 'other'
+            ],
+            'defaultPipeline' => 'async'
+        ]);
+
+        $this->assertSame('other', $config->jobPipeline(LocalJob::class));
+        $this->assertSame('async', $config->jobPipeline(self::class));
+    }
+}
