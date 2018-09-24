cdd289d0dab7a5580b05aac349a36f976e096bb4
Wolfy-J <wolfy.jd@gmail.com>
2018-09-24 22:44:56 +0300

    sqs

--- broker/sqs/queue.go
+++ broker/sqs/queue.go
@@ -3,7 +3,6 @@ package sqs
 import (
 	"errors"
 	"github.com/spiral/jobs"
-	"github.com/aws/aws-sdk-go/aws"
 )
 
 // Queue defines single SQS queue.
@@ -16,6 +15,9 @@ type Queue struct {
 
 	// Create indicates that queue must be automatically created.
 	Create bool
+
+	// Configure defines set of options to be used to create queue.
+	Configure map[interface{}]interface{}
 
 	// Indicates that tube must be listened.
 	Listen bool
@@ -31,11 +33,18 @@ type Queue struct {
 }
 
 // CreateAttributes must return queue create attributes.
-func (q *Queue) CreateAttributes() map[string]*string {
-	// todo: add more attributes
-	return map[string]*string{
-		"MessageRetentionPeriod": aws.String("86400"),
+func (q *Queue) CreateAttributes() (attr map[string]*string) {
+	attr = make(map[string]*string)
+
+	for k, v := range q.Configure {
+		if ks, ok := k.(string); ok {
+			if vs, ok := v.(string); ok {
+				attr[ks] = &vs
+			}
+		}
 	}
+
+	return attr
 }
 
 // NewTube creates new tube or returns an error
@@ -48,12 +57,20 @@ func NewQueue(p *jobs.Pipeline) (*Queue, error) {
 		return nil, errors.New("invalid `threads` value for sqs pipeline, must be 1 or greater")
 	}
 
-	return &Queue{
+	q := &Queue{
 		Queue:    p.Options.String("queue", ""),
-		Create:   p.Options.Bool("create", false),
+		Create:   p.Options.Bool("create", true),
 		Listen:   p.Listen,
 		Timeout:  p.Options.Integer("timeout", 600),
 		WaitTime: p.Options.Integer("waitTime", 1),
 		Threads:  p.Options.Integer("threads", 1),
-	}, nil
+	}
+
+	if configureOptions, ok := p.Options["configure"]; ok {
+		if configure, ok := configureOptions.(map[interface{}]interface{}); ok {
+			q.Configure = configure
+		}
+	}
+
+	return q, nil
 }
