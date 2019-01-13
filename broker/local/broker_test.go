427af164ef22bcd55f3660a4332fad0d532370bc
Wolfy-J <wolfy.jd@gmail.com>
2019-01-13 15:52:07 +0300

    more tests, yeeah

---
+++ broker/local/broker_test.go
@@ -0,0 +1,71 @@
+package local
+
+import (
+	"github.com/spiral/jobs"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+var (
+	pipe = &jobs.Pipeline{
+		"broker": "local",
+		"name":   "default",
+	}
+)
+
+func TestBroker_Init(t *testing.T) {
+	b := &Broker{}
+	ok, err := b.Init()
+	assert.True(t, ok)
+	assert.NoError(t, err)
+}
+
+func TestBroker_Register(t *testing.T) {
+	b := &Broker{}
+	b.Init()
+	assert.NoError(t, b.Register(pipe))
+}
+
+func TestBroker_Register_Twice(t *testing.T) {
+	b := &Broker{}
+	b.Init()
+	assert.NoError(t, b.Register(pipe))
+	assert.Error(t, b.Register(pipe))
+}
+
+func TestBroker_Consume_Nil_BeforeServe(t *testing.T) {
+	b := &Broker{}
+	b.Init()
+	b.Register(pipe)
+	assert.NoError(t, b.Consume(pipe, nil, nil))
+}
+
+func TestBroker_Consume_BeforeServe(t *testing.T) {
+	b := &Broker{}
+	b.Init()
+	b.Register(pipe)
+
+	exec := make(chan jobs.Handler)
+	err := func(id string, j *jobs.Job, err error) {}
+
+	assert.NoError(t, b.Consume(pipe, exec, err))
+}
+
+func TestBroker_Consume_Serve_Nil_Stop(t *testing.T) {
+	b := &Broker{}
+	b.Init()
+	b.Register(pipe)
+
+	b.Consume(pipe, nil, nil)
+
+	wait := make(chan interface{})
+	go func() {
+		assert.NoError(t, b.Serve())
+		close(wait)
+	}()
+	time.Sleep(time.Microsecond * 100)
+	b.Stop()
+
+	<-wait
+}
