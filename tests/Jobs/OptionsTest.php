6986604dbcebbc498efbe80613fb594572675a60
Wolfy-J <wolfy.jd@gmail.com>
2019-01-14 17:25:36 +0300

    go fmt

--- tests/Jobs/OptionsTest.php
+++ tests/Jobs/OptionsTest.php
@@ -13,11 +13,19 @@ use Spiral\Jobs\Options;
 
 class OptionsTest extends TestCase
 {
-    public function testOptions()
+    public function testDelay()
     {
         $o = new Options();
         $this->assertNull($o->getDelay());
         $o = $o->withDelay(10);
         $this->assertSame(10, $o->getDelay());
+    }
+
+    public function testPipeline()
+    {
+        $o = new Options();
+        $this->assertNull($o->getPipeline());
+        $o = $o->withPipeline("custom");
+        $this->assertSame("custom", $o->getPipeline());
     }
 }
