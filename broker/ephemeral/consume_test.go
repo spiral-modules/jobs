4a02dd54950f7c01b8706504ba77048d6b6debe2
Wolfy-J <wolfy.jd@gmail.com>
2019-01-14 16:30:34 +0300

    test delayed jobs

--- broker/ephemeral/consume_test.go
+++ broker/ephemeral/consume_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/spiral/jobs"
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 func TestBroker_Consume_Job(t *testing.T) {
@@ -84,4 +85,49 @@ func TestBroker_ConsumeAfterStart_Job(t *testing.T) {
 	}
 
 	<-waitJob
+}
+
+func TestBroker_Consume_Delayed(t *testing.T) {
+	b := &Broker{}
+	b.Init()
+	b.Register(pipe)
+
+	ready := make(chan interface{})
+	b.Listen(func(event int, ctx interface{}) {
+		if event == jobs.EventBrokerReady {
+			close(ready)
+		}
+	})
+
+	exec := make(chan jobs.Handler, 1)
+	assert.NoError(t, b.Consume(pipe, exec, func(id string, j *jobs.Job, err error) {}))
+
+	go func() { assert.NoError(t, b.Serve()) }()
+	defer b.Stop()
+
+	<-ready
+
+	jid, perr := b.Push(pipe, &jobs.Job{
+		Job:     "test",
+		Payload: "body",
+		Options: &jobs.Options{Delay: 1},
+	})
+
+	assert.NotEqual(t, "", jid)
+	assert.NoError(t, perr)
+
+	waitJob := make(chan interface{})
+	start := time.Now()
+	exec <- func(id string, j *jobs.Job) error {
+		assert.Equal(t, jid, id)
+		assert.Equal(t, "body", j.Payload)
+		close(waitJob)
+		return nil
+	}
+
+	<-waitJob
+
+	elapsed := time.Since(start)
+	assert.True(t, elapsed > time.Second)
+	assert.True(t, elapsed < 2*time.Second)
 }
