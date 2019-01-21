2ae791d7091195c69ade22d14d9a911e6b791efa
Wolfy-J <wolfy.jd@gmail.com>
2019-01-21 15:46:08 +0300

    more tests

--- rpc_test.go
+++ rpc_test.go
@@ -5,6 +5,8 @@ import (
 	"github.com/spiral/roadrunner/service"
 	"github.com/spiral/roadrunner/service/rpc"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"syscall"
 	"testing"
 )
 
@@ -208,4 +210,63 @@ func TestRPC_DisableConsuming(t *testing.T) {
 
 	assert.Equal(t, int64(0), list.Pipelines[0].Queue)
 	assert.Equal(t, false, list.Pipelines[0].Consuming)
+}
+
+func TestRPC_DoJob(t *testing.T) {
+	c := service.NewContainer(logrus.New())
+	c.Register("rpc", &rpc.Service{})
+	c.Register("jobs", &Service{Brokers: map[string]Broker{"ephemeral": &testBroker{}}})
+
+	assert.NoError(t, c.Init(viperConfig(`{
+	"rpc":{"listen":"tcp://:5004"},
+	"jobs":{
+		"workers":{
+			"command": "php tests/consumer.php",
+			"pool.numWorkers": 1
+		},
+		"pipelines":{"default":{"broker":"ephemeral"}},
+    	"dispatch": {
+	    	"spiral-jobs-tests-local-*.pipeline": "default"
+    	},
+    	"consume": ["default"]
+	}
+}`)))
+
+	ready := make(chan interface{})
+	jobReady := make(chan interface{})
+	jobs(c).AddListener(func(event int, ctx interface{}) {
+		if event == EventBrokerReady {
+			close(ready)
+		}
+
+		if event == EventJobComplete {
+			close(jobReady)
+		}
+	})
+
+	go func() { c.Serve() }()
+	defer c.Stop()
+	<-ready
+
+	s2, _ := c.Get(rpc.ID)
+	rs := s2.(*rpc.Service)
+
+	cl, err := rs.Client()
+	assert.NoError(t, err)
+
+	id := ""
+	assert.NoError(t, cl.Call("jobs.Push", &Job{
+		Job:     "spiral.jobs.tests.local.job",
+		Payload: `{"data":100}`,
+		Options: &Options{},
+	}, &id))
+	assert.NoError(t, err)
+
+	<-jobReady
+
+	data, err := ioutil.ReadFile("tests/local.job")
+	assert.NoError(t, err)
+	defer syscall.Unlink("tests/local.job")
+
+	assert.Contains(t, string(data), id)
 }
