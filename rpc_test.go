3e2e532d5c75baf572fa9b065747506943604b80
Wolfy-J <wolfy.jd@gmail.com>
2019-01-21 15:35:48 +0300

    rpc tests

--- rpc_test.go
+++ rpc_test.go
@@ -1,105 +1,20 @@
 package jobs
 
 import (
-	"bytes"
 	"github.com/sirupsen/logrus"
-	"github.com/spf13/viper"
 	"github.com/spiral/roadrunner/service"
+	"github.com/spiral/roadrunner/service/rpc"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func viperConfig(cfg string) service.Config {
-	v := viper.New()
-	v.SetConfigType("json")
-
-	err := v.ReadConfig(bytes.NewBuffer([]byte(cfg)))
-	if err != nil {
-		panic(err)
-	}
-
-	return &configWrapper{v}
-}
-
-// configWrapper provides interface bridge between v configs and service.Config.
-type configWrapper struct {
-	v *viper.Viper
-}
-
-// Get nested config section (sub-map), returns nil if section not found.
-func (w *configWrapper) Get(key string) service.Config {
-	sub := w.v.Sub(key)
-	if sub == nil {
-		return nil
-	}
-
-	return &configWrapper{sub}
-}
-
-// Unmarshal unmarshal config data into given struct.
-func (w *configWrapper) Unmarshal(out interface{}) error {
-	return w.v.Unmarshal(out)
-}
-
-func jobs(container service.Container) *Service {
-	svc, _ := container.Get("jobs")
-	return svc.(*Service)
-}
-
-func TestService_Init(t *testing.T) {
+func TestRPC_StatPipeline(t *testing.T) {
 	c := service.NewContainer(logrus.New())
+	c.Register("rpc", &rpc.Service{})
 	c.Register("jobs", &Service{Brokers: map[string]Broker{"ephemeral": &testBroker{}}})
 
 	assert.NoError(t, c.Init(viperConfig(`{
-	"jobs":{
-		"workers":{
-			"command": "php tests/consumer.php",
-			"pool.numWorkers": 1
-		},
-		"pipelines":{"default":{"broker":"ephemeral"}},
-    	"dispatch": {
-	    	"spiral-jobs-tests-local-*.pipeline": "default"
-    	},
-    	"consume": ["default"]
-	}
-}`)))
-}
-
-func TestService_ServeStop(t *testing.T) {
-	c := service.NewContainer(logrus.New())
-	c.Register("jobs", &Service{Brokers: map[string]Broker{"ephemeral": &testBroker{}}})
-
-	assert.NoError(t, c.Init(viperConfig(`{
-	"jobs":{
-		"workers":{
-			"command": "php tests/consumer.php",
-			"pool.numWorkers": 1
-		},
-		"pipelines":{"default":{"broker":"ephemeral"}},
-    	"dispatch": {
-	    	"spiral-jobs-tests-local-*.pipeline": "default"
-    	},
-    	"consume": ["default"]
-	}
-}`)))
-
-	ready := make(chan interface{})
-	jobs(c).AddListener(func(event int, ctx interface{}) {
-		if event == EventBrokerReady {
-			close(ready)
-		}
-	})
-
-	go func() { c.Serve() }()
-	<-ready
-	c.Stop()
-}
-
-func TestService_GetPipeline(t *testing.T) {
-	c := service.NewContainer(logrus.New())
-	c.Register("jobs", &Service{Brokers: map[string]Broker{"ephemeral": &testBroker{}}})
-
-	assert.NoError(t, c.Init(viperConfig(`{
+	"rpc":{"listen":"tcp://:5004"},
 	"jobs":{
 		"workers":{
 			"command": "php tests/consumer.php",
@@ -124,53 +39,28 @@ func TestService_GetPipeline(t *testing.T) {
 	defer c.Stop()
 	<-ready
 
-	assert.Equal(t, "ephemeral", jobs(c).Pipelines().Get("default").Broker())
-}
+	s2, _ := c.Get(rpc.ID)
+	rs := s2.(*rpc.Service)
 
-func TestService_StatPipeline(t *testing.T) {
-	c := service.NewContainer(logrus.New())
-	c.Register("jobs", &Service{Brokers: map[string]Broker{"ephemeral": &testBroker{}}})
-
-	assert.NoError(t, c.Init(viperConfig(`{
-	"jobs":{
-		"workers":{
-			"command": "php tests/consumer.php",
-			"pool.numWorkers": 1
-		},
-		"pipelines":{"default":{"broker":"ephemeral"}},
-    	"dispatch": {
-	    	"spiral-jobs-tests-local-*.pipeline": "default"
-    	},
-    	"consume": ["default"]
-	}
-}`)))
-
-	ready := make(chan interface{})
-	jobs(c).AddListener(func(event int, ctx interface{}) {
-		if event == EventBrokerReady {
-			close(ready)
-		}
-	})
-
-	go func() { c.Serve() }()
-	defer c.Stop()
-	<-ready
-
-	svc := jobs(c)
-	pipe := svc.Pipelines().Get("default")
-
-	stat, err := svc.Stat(pipe)
+	cl, err := rs.Client()
 	assert.NoError(t, err)
 
-	assert.Equal(t, int64(0), stat.Queue)
-	assert.Equal(t, true, stat.Consuming)
+	list := &PipelineList{}
+	assert.NoError(t, cl.Call("jobs.Stat", true, &list))
+
+	assert.Len(t, list.Pipelines, 1)
+
+	assert.Equal(t, int64(0), list.Pipelines[0].Queue)
+	assert.Equal(t, true, list.Pipelines[0].Consuming)
 }
 
-func TestService_StatNonConsumingPipeline(t *testing.T) {
+func TestRPC_StatNonActivePipeline(t *testing.T) {
 	c := service.NewContainer(logrus.New())
+	c.Register("rpc", &rpc.Service{})
 	c.Register("jobs", &Service{Brokers: map[string]Broker{"ephemeral": &testBroker{}}})
 
 	assert.NoError(t, c.Init(viperConfig(`{
+	"rpc":{"listen":"tcp://:5004"},
 	"jobs":{
 		"workers":{
 			"command": "php tests/consumer.php",
@@ -195,12 +85,127 @@ func TestService_StatNonConsumingPipeline(t *testing.T) {
 	defer c.Stop()
 	<-ready
 
-	svc := jobs(c)
-	pipe := svc.Pipelines().Get("default")
+	s2, _ := c.Get(rpc.ID)
+	rs := s2.(*rpc.Service)
 
-	stat, err := svc.Stat(pipe)
+	cl, err := rs.Client()
 	assert.NoError(t, err)
 
-	assert.Equal(t, int64(0), stat.Queue)
-	assert.Equal(t, false, stat.Consuming)
+	list := &PipelineList{}
+	assert.NoError(t, cl.Call("jobs.Stat", true, &list))
+
+	assert.Len(t, list.Pipelines, 1)
+
+	assert.Equal(t, int64(0), list.Pipelines[0].Queue)
+	assert.Equal(t, false, list.Pipelines[0].Consuming)
+}
+
+func TestRPC_EnableConsuming(t *testing.T) {
+	c := service.NewContainer(logrus.New())
+	c.Register("rpc", &rpc.Service{})
+	c.Register("jobs", &Service{Brokers: map[string]Broker{"ephemeral": &testBroker{}}})
+
+	assert.NoError(t, c.Init(viperConfig(`{
+	"rpc":{"listen":"tcp://:5004"},
+	"jobs":{
+		"workers":{
+			"command": "php tests/consumer.php",
+			"pool.numWorkers": 1
+		},
+		"pipelines":{"default":{"broker":"ephemeral"}},
+    	"dispatch": {
+	    	"spiral-jobs-tests-local-*.pipeline": "default"
+    	},
+    	"consume": []
+	}
+}`)))
+
+	ready := make(chan interface{})
+	pipelineReady := make(chan interface{})
+	jobs(c).AddListener(func(event int, ctx interface{}) {
+		if event == EventBrokerReady {
+			close(ready)
+		}
+
+		if event == EventPipelineConsuming {
+			close(pipelineReady)
+		}
+	})
+
+	go func() { c.Serve() }()
+	defer c.Stop()
+	<-ready
+
+	s2, _ := c.Get(rpc.ID)
+	rs := s2.(*rpc.Service)
+
+	cl, err := rs.Client()
+	assert.NoError(t, err)
+
+	assert.NoError(t, cl.Call("jobs.Resume", "default", nil))
+
+	<-pipelineReady
+
+	list := &PipelineList{}
+	assert.NoError(t, cl.Call("jobs.Stat", true, &list))
+
+	assert.Len(t, list.Pipelines, 1)
+
+	assert.Equal(t, int64(0), list.Pipelines[0].Queue)
+	assert.Equal(t, true, list.Pipelines[0].Consuming)
+}
+
+func TestRPC_DisableConsuming(t *testing.T) {
+	c := service.NewContainer(logrus.New())
+	c.Register("rpc", &rpc.Service{})
+	c.Register("jobs", &Service{Brokers: map[string]Broker{"ephemeral": &testBroker{}}})
+
+	assert.NoError(t, c.Init(viperConfig(`{
+	"rpc":{"listen":"tcp://:5004"},
+	"jobs":{
+		"workers":{
+			"command": "php tests/consumer.php",
+			"pool.numWorkers": 1
+		},
+		"pipelines":{"default":{"broker":"ephemeral"}},
+    	"dispatch": {
+	    	"spiral-jobs-tests-local-*.pipeline": "default"
+    	},
+    	"consume": ["default"]
+	}
+}`)))
+
+	ready := make(chan interface{})
+	pipelineReady := make(chan interface{})
+	jobs(c).AddListener(func(event int, ctx interface{}) {
+		if event == EventBrokerReady {
+			close(ready)
+		}
+
+		if event == EventPipelineStopped {
+			close(pipelineReady)
+		}
+	})
+
+	go func() { c.Serve() }()
+	defer c.Stop()
+	<-ready
+
+	s2, _ := c.Get(rpc.ID)
+	rs := s2.(*rpc.Service)
+
+	cl, err := rs.Client()
+	assert.NoError(t, err)
+
+	assert.NoError(t, cl.Call("jobs.Stop", "default", nil))
+
+	<-pipelineReady
+
+	list := &PipelineList{}
+	assert.NoError(t, cl.Call("jobs.Stat", true, &list))
+
+	assert.Len(t, list.Pipelines, 1)
+
+	assert.Equal(t, int64(0), list.Pipelines[0].Queue)
+	assert.Equal(t, false, list.Pipelines[0].Consuming)
 }
