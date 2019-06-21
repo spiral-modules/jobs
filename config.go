5794659e2a31339c72bf3dd837154f51485a49a6
Wolfy-J <wolfy.jd@gmail.com>
2019-06-21 11:09:20 +0300

    ability to working in produce only mode

--- config.go
+++ config.go
@@ -40,7 +40,7 @@ func (c *Config) Hydrate(cfg service.Config) (err error) {
 		return err
 	}
 
-	if c.Workers.Pool != nil {
+	if c.Workers.Command != "" {
 		if err := c.Workers.Pool.Valid(); err != nil {
 			return c.Workers.Pool.Valid()
 		}
